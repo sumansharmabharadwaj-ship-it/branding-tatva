0213641344e9c0d54ac6a636b4133fae015bd729
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-05 07:00:31 +0530

    Add the homepage film playback heartbeat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import { ChapterLadder } from "@/sections/Home/ChapterLadder";
 import { HomeFilmConstellation } from "@/sections/Home/HomeFilmConstellation";
 import { HomeAutoJourney } from "@/sections/Home/HomeAutoJourney";
 import { HomeChapterDirector } from "@/sections/Home/HomeChapterDirector";
+import { HomeVideoHeartbeat } from "@/sections/Home/HomeVideoHeartbeat";
 import { site } from "@/data/site";
 
 const displayFont = Cormorant_Garamond({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-display", display: "swap" });
@@ -52,6 +53,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <HomeFilmConstellation />
             <HomeChapterDirector />
             <HomeAutoJourney />
+            <HomeVideoHeartbeat />
             <ChapterLadder />
           </MotionPreferenceProvider>
         </SmoothScrollProvider>
