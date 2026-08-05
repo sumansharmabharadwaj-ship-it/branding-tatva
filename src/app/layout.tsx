da4f3179d876e7a5308b88086ca5e21b46edfe27
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-05 06:41:34 +0530

    Load the homepage Mother Earth atmosphere system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Cormorant_Garamond, Manrope } from "next/font/google";
 import "./globals.css";
+import "./home-earth-tones.css";
 import { PageLoadVeil } from "@/components/PageLoadVeil";
 import { AmbientAudio } from "@/components/AmbientAudio";
 import { SmoothScrollProvider } from "@/components/SmoothScrollProvider";
