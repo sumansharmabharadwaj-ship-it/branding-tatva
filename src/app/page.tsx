347c8e68d6a83786263a5ff66f548cba01646c1d
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-06 16:50:04 +0530

    Load the Studio author-focused portrait framing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import "./home-v4-mobile-polish.css";
 import "./home-v4-tatva-depth.css";
 import "./home-v4-tatva-mobile-fix.css";
 import "./home-v4-studio-depth.css";
+import "./home-v4-studio-portrait-fix.css";
 import { Header } from "@/layouts/Header";
 import { Footer } from "@/sections/Footer";
 import { HomeV4Experience } from "@/sections/HomeV4/HomeV4Experience";
