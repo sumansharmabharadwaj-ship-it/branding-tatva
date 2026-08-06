cc2f18595b6fba5d68290421df761fa9d199c1cf
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-06 22:15:18 +0530

    Point legacy Insight library at canonical data

--- src/data/insightLibrary.ts
+++ src/data/insightLibrary.ts
@@ -1,37 +1,8 @@
-import { consultingPositioningInsightPosts } from "@/data/consultingPositioningInsights";
-import { customerJourneyInsightPosts } from "@/data/customerJourneyInsights";
-import { distinctiveAssetInsightPosts } from "@/data/distinctiveAssetInsights";
-import {
-  insightPosts as pillarInsightPosts,
+export {
+  getInsightBySlug,
+  getInsightTopic,
+  getInsightsByTopic,
+  insightPosts,
   insightTopics,
-  type InsightPost,
 } from "@/data/insights";
-import { supportingInsightPosts } from "@/data/supportingInsights";
-
-export { insightTopics };
-export type { InsightPost };
-
-const combinedPosts = [
-  ...pillarInsightPosts,
-  ...supportingInsightPosts,
-  ...distinctiveAssetInsightPosts,
-  ...customerJourneyInsightPosts,
-  ...consultingPositioningInsightPosts,
-];
-
-export const insightPosts: InsightPost[] = combinedPosts.filter(
-  (post, index) =>
-    combinedPosts.findIndex((candidate) => candidate.slug === post.slug) === index
-);
-
-export function getInsightBySlug(slug: string) {
-  return insightPosts.find((post) => post.slug === slug);
-}
-
-export function getInsightTopic(slug: string) {
-  return insightTopics.find((topic) => topic.slug === slug);
-}
-
-export function getInsightsByTopic(topicSlug: string) {
-  return insightPosts.filter((post) => post.topicSlug === topicSlug);
-}
+export type { InsightPost } from "@/data/insights";
