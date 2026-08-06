5a18d75cece1266091a58087da4703fb442c75d0
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-06 16:15:13 +0530

    Load the Tatva phone containment pass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import "./home-v4-living-type.css";
 import "./home-v4-paths-depth.css";
 import "./home-v4-mobile-polish.css";
 import "./home-v4-tatva-depth.css";
+import "./home-v4-tatva-mobile-fix.css";
 import { Header } from "@/layouts/Header";
 import { Footer } from "@/sections/Footer";
 import { HomeV4Experience } from "@/sections/HomeV4/HomeV4Experience";
