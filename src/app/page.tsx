721898377474913a68f60883029fb0dd90a38682
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-06 15:19:06 +0530

    Load living typography across the cinematic homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import "./home-final-polish.css";
 import "./home-v4-continuity.css";
 import "./home-v4-guided-motion.css";
 import "./home-v4-recognition-depth.css";
+import "./home-v4-living-type.css";
 import { Header } from "@/layouts/Header";
 import { Footer } from "@/sections/Footer";
 import { HomeV4Experience } from "@/sections/HomeV4/HomeV4Experience";
