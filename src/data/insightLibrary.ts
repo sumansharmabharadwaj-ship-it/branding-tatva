8dd37c1f925d41ba0ef6986c4fa2ac3054530e15
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-06 22:13:18 +0530

    Register consulting positioning guide

--- src/data/insightLibrary.ts
+++ src/data/insightLibrary.ts
@@ -1,3 +1,4 @@
+import { consultingPositioningInsightPosts } from "@/data/consultingPositioningInsights";
 import { customerJourneyInsightPosts } from "@/data/customerJourneyInsights";
 import { distinctiveAssetInsightPosts } from "@/data/distinctiveAssetInsights";
 import {
@@ -15,6 +16,7 @@ const combinedPosts = [
   ...supportingInsightPosts,
   ...distinctiveAssetInsightPosts,
   ...customerJourneyInsightPosts,
+  ...consultingPositioningInsightPosts,
 ];
 
 export const insightPosts: InsightPost[] = combinedPosts.filter(
