abb11d250d2ceac884106b00cc95b508d7577657
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-06 15:16:58 +0530

    Register Work filter analytics event

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -9,6 +9,7 @@ import { track as vercelTrack } from "@vercel/analytics";
 export type AnalyticsEvent =
   | "hero_booking_click"
   | "case_study_opened"
+  | "work_filter_selected"
   | "capability_selected"
   | "deliverable_inspected"
   | "contextual_cta_clicked"
