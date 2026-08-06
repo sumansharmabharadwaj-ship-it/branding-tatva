26b623671dd9cedd899f6ca90298aa2d355045d2
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-08-07 02:51:50 +0530

    Mount the compressed Tatva tempo director

--- src/sections/HomeV4/HomeV4Experience.tsx
+++ src/sections/HomeV4/HomeV4Experience.tsx
@@ -20,6 +20,7 @@ import { HomeV4MediaDirector } from "./HomeV4MediaDirector";
 import { HomeV4PreludeBridge } from "./HomeV4PreludeBridge";
 import { HomeV4ProcessTempo } from "./HomeV4ProcessTempo";
 import { HomeV4RecognitionTempo } from "./HomeV4RecognitionTempo";
+import { HomeV4TatvaTempo } from "./HomeV4TatvaTempo";
 import { V4HiddenCostScene, V4OpeningScene, V4RecognitionScene } from "./HomeV4Scenes";
 
 export function HomeV4Experience() {
@@ -35,6 +36,7 @@ export function HomeV4Experience() {
       <HomeV4HeaderDirector />
       <HomeV4RecognitionTempo />
       <HomeV4ProcessTempo />
+      <HomeV4TatvaTempo />
       <LivingCursor />
       <GuidedView />
 
