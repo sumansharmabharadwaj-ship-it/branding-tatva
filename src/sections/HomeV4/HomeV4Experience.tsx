baba8b06fe2662a4dbf8adc06daff8d6f440e063
sumansharmabharadwaj-ship-it <sumansharma.bharadwaj@gmail.com>
2026-09-09 16:01:09 +0530

    Refine the homepage working method with deliberate stage choices [deploy] [release:124]

--- src/sections/HomeV4/HomeV4Experience.tsx
+++ src/sections/HomeV4/HomeV4Experience.tsx
@@ -17,7 +17,6 @@ import { HomeV4HeaderDirector } from "./HomeV4HeaderDirector";
 import { GuidedView, LivingCursor, SceneHandoff } from "./HomeV4Interface";
 import { HomeV4MediaDirector } from "./HomeV4MediaDirector";
 import { HomeV4PreludeBridge } from "./HomeV4PreludeBridge";
-import { HomeV4ProcessTempo } from "./HomeV4ProcessTempo";
 import { V4HiddenCostScene, V4OpeningScene, V4RecognitionScene } from "./HomeV4Scenes";
 
 export function HomeV4Experience() {
@@ -31,7 +30,6 @@ export function HomeV4Experience() {
       <HomeV4PreludeBridge />
       <HomeV4MediaDirector />
       <HomeV4HeaderDirector />
-      <HomeV4ProcessTempo />
       <LivingCursor />
       <GuidedView />
 
